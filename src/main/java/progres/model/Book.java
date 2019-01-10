f72a924175ce66806277fe63728a28195c80690f
krzysztof.fitas <krzysztof.fitas@comarch.com>
2019-01-10 15:05:42 +0100

    some init msg

---
+++ src/main/java/progres/model/Book.java
@@ -0,0 +1,13 @@
+package progres.model;
+
+import lombok.Data;
+import javax.persistence.Entity;
+
+@Entity
+@Data
+public class Book extends BaseEntity {
+
+    private String bookName;
+    private String authorName;
+    private String authorSurname;
+}
