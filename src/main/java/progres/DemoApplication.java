d72f7aa3de0520ddd000ac285ebca00000bca27a
Krzysztof Fitas <krzysiek.fitas91@gmail.com>
2019-02-07 15:04:39 +0100

    testowa dansa stara01

--- src/main/java/progres/DemoApplication.java
+++ src/main/java/progres/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    //testowa dansa stara
+    //testowa dansa stara01
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
